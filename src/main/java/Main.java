eb0b07b781c0026da687e9f18a2b8fc653ca7902
3yebmb <3yebmb@gmail.com>
2019-02-19 01:13:29 +0500

    midnight code is not completed. to be continued...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //
+        Integer[] orig_arr = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        System.out.println("Исходный массив : " + Arrays.asList(orig_arr));
+        System.out.println("Обработанный массив : " + Arrays.asList(backArrAfterLastFour(orig_arr)));
+    }
+
+    public static <T extends Number> T[] backArrAfterLastFour(T[] arr) throws MyException {
+        T[] result = null;
+        ArrayList<T> alist = new ArrayList<T>();
+
+        for (int i = arr.length; i > 0 ; i--) {
+            if (arr[i].equals(4)) break;
+//            else result[arr.length-i]
+        }
+
+        result = arr;
+
+        return result;
+    }
+
+    class MyException extends RuntimeException {
+        public MyException(String message) {
+            super(message);
+        }
     }
 }
