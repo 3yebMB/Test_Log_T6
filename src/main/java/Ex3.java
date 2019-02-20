f661b226474a7b14714b552678bef881f3cb9c13
Michael <3yebmb@gmail.com>
2019-02-20 11:52:19 +0500

    ex.3 has been done.

--- src/main/java/Ex3.java
+++ src/main/java/Ex3.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class Ex3 {
+    public static void main(String[] args) {
+        int[] arr = {1,1,4,4,4,4,1,1,4,1,4,4,1};
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(checkArray(arr));
+    }
+
+    public static boolean checkArray(int[] mas) {
+        boolean one = false;
+        boolean four = false;
+
+        for (int i = 0; i < mas.length; i++) {
+            if (one & four) break;
+            if (mas[i] == 1) one = true;
+            if (mas[i] == 4) four = true;
+        }
+
+        return (one && four)?(true):(false);
+    }
 }
