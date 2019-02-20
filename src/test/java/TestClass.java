0e6700c63298cf9b00984f5c83b4cd1fa966ea9d
Michael <3yebmb@gmail.com>
2019-02-20 11:52:49 +0500

    changed tests

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,9 +1,13 @@
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TestClass {
     @Test
-    public void test1 () {
+    public void test2 () {
+
+    }
+
+    @Test
+    public void test3 () {
 
     }
 }
